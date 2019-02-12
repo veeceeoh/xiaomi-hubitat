44f445fbde9250d39ebbfc3f19e88bc4ce8732c8
Keith G <33558908+veeceeoh@users.noreply.github.com>
2019-02-11 23:03:56 -0800

    [UPDATE] v0.6.5
    Changelist
    * Added compatibility fix for Hubitat firmware 2.0.5 and newer
    * Added preference setting to disable firmware 2.0.5 compatibility fix

--- devicedrivers/xiaomi-aqara-button-hubitat.src/xiaomi-aqara-button-hubitat.groovy
+++ devicedrivers/xiaomi-aqara-button-hubitat.src/xiaomi-aqara-button-hubitat.groovy
@@ -100,15 +100,16 @@ def parse(String description) {
 	def valueHex = description.split(",").find {it.split(":")[0].trim() == "value"}?.split(":")[1].trim()
 	Map map = [:]
 
-	if (!oldFirmware & valueHex)
+	if (!oldFirmware & valueHex != null)
 		// Reverse order of bytes in description's value hex string - required for Hubitat firmware 2.0.5 or newer
 		valueHex = reverseHexString(valueHex)
+
+	displayDebugLog("Parsing message: ${description}")
+	displayDebugLog("Message payload: ${valueHex}")
 
 	// lastCheckinEpoch is for apps that can use Epoch time/date and lastCheckinTime can be used with Hubitat Dashboard
 	sendEvent(name: "lastCheckinEpoch", value: now())
 	sendEvent(name: "lastCheckinTime", value: new Date().toLocaleString())
-
-	displayDebugLog("Parsing message: ${description}")
 
 	// Send message data to appropriate parsing function based on the type of report
 	if (cluster == "0006") {
@@ -144,7 +145,7 @@ def parse(String description) {
 def reverseHexString(hexString) {
 	def reversed = ""
 	for (int i = hexString.length(); i > 0; i -= 2) {
-		swaped += hexString.substring(i - 2, i )
+		reversed += hexString.substring(i - 2, i )
 	}
 	return reversed
 }
