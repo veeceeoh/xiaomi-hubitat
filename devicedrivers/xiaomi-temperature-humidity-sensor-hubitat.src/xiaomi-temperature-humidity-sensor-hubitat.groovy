ff280f80f2bc3e599edecc26cb5e0e8b4c65e995
Keith G <33558908+veeceeoh@users.noreply.github.com>
2020-01-24 17:15:32 -0800

    [UPDATE] v1.0
    Changelist
    * Added parsing of "check-in report" messages sent every 50-60 minutes:
    	* Generate events for check-in reports for sensor readings: temperature / humidity (/ pressure - Aqara only). Note that all check-in sensor reading events are generated even if the value is unchanged from the most recent event. Check-in sensor reading events can be recognized by the addition of "(check-in report) at the end of their event description text."
    	* Log output and update State Variables for reported RSSI dB, LQI, and ZigBee Parent device network ID (DNI)
    * Added new preference settings:
    	* Added toggle to enable generation of events for ALL sensor readings that occur between check-in reports. The default is the normal Hubitat driver behavior of NOT generating events for unchanged sensor reading values, with the exception of check-in reports.
    	* Added toggle to enable generation of events for ALL battery reports. The default is the normal Hubitat driver behavior of NOT generating events for unchanged battery values.
    * Debug and Info messages are now automatically logged for the first two hours after the device is paired or re-joined. A State Variable is no longer used for the purpose of automatic logging at device pairing.
    * Refactored and changed method of generating events [all events are now generated using `sendEvent()`]

--- devicedrivers/xiaomi-temperature-humidity-sensor-hubitat.src/xiaomi-temperature-humidity-sensor-hubitat.groovy
+++ devicedrivers/xiaomi-temperature-humidity-sensor-hubitat.src/xiaomi-temperature-humidity-sensor-hubitat.groovy
@@ -2,7 +2,7 @@
  * Xiaomi "Original" Temperature Humidity Sensor - model RTCGQ01LM
  * & Aqara Temperature Humidity Sensor - model WSDCGQ11LM
  * Device Driver for Hubitat Elevation hub
- * Version 0.9
+ * Version 1.0
  *
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -22,11 +22,11 @@
  * + Xiaomi devices send reports based on changes, and a status report every 50-60 minutes. These settings cannot be adjusted.
  * + The battery level / voltage is not reported at pairing. Wait for the first status report, 50-60 minutes after pairing.
  * + Pairing Xiaomi devices can be difficult as they were not designed to use with a Hubitat hub.
- *  Holding the sensor's reset button until the LED blinks will start pairing mode.
- *  3 quick flashes indicates success, while one long flash means pairing has not started yet.
- *  In either case, keep the sensor "awake" by short-pressing the reset button repeatedly, until recognized by Hubitat.
+ *   Holding the sensor's reset button until the LED blinks will start pairing mode.
+ *   3 quick flashes indicates success, while one long flash means pairing has not started yet.
+ *   In either case, keep the sensor "awake" by short-pressing the reset button repeatedly, until recognized by Hubitat.
  * + The connection can be dropped without warning. To reconnect, put Hubitat in "Discover Devices" mode, then short-press
- *  the sensor's reset button, and wait for the LED - 3 quick flashes indicates reconnection. Otherwise, short-press again.
+ *   the sensor's reset button, and wait for the LED - 3 quick flashes indicates reconnection. Otherwise, short-press again.
  *
  */
 
@@ -64,6 +64,9 @@ metadata {
 		input name: "voltsmax", type: "decimal", title: "Max Volts (100% battery = ___ volts). Default = 3.05 Volts", description: ""
 		//Date/Time Stamp Events Config
 		input name: "lastCheckinEnable", type: "bool", title: "Enable custom date/time stamp events for lastCheckin", description: ""
+		//Event Generation Behavior Config
+		input name: "recordAllReadings", type: "bool", title: "Enable events for unchanged sensor readings between hourly check-in", description: ""
+		input name: "recordAllBattery", type: "bool", title: "Enable events for unchanged battery level", description: ""
 		//Logging Message Config
 		input name: "infoLogging", type: "bool", title: "Enable info message logging", description: ""
 		input name: "debugLogging", type: "bool", title: "Enable debug message logging", description: ""
@@ -73,7 +76,6 @@ metadata {
 // Parse incoming device messages to generate events
 def parse(String description) {
 	displayDebugLog("Parsing message: $description")
-	def result
 	if (description?.startsWith('cat')) {
 		Map descMap = zigbee.parseDescriptionAsMap(description)
 		displayDebugLog("Zigbee parse map of catchall = $descMap")
@@ -97,28 +99,22 @@ def parse(String description) {
 				if (descMap.attrId == "0005")
 					displayDebugLog("Reset button was short-pressed")
 				else if (descMap.attrId == "FF01" || descMap.attrId == "FF02") // Hourly check-in report
-					result = parseCheckinMessage(descMap.value)
+					parseCheckinMessage(descMap.value)
 				break
 			case "0402": // Temperature report
-				result = parseTemperature(descMap.value)
+				parseTemperature(descMap.value, false)
 				break
 			case "0403": // Pressure report (Aqara model only)
-				result = parsePressure(descMap.value)
+				parsePressure(Integer.parseInt(descMap.value[0..3], 16)*10, false)
 				break
 			case "0405": // Humidity report
-				result = parseHumidity(descMap.value)
+				parseHumidity(descMap.value, false)
 				break
 			default:
 				displayDebugLog("Unknown read attribute message")
 		}
 	}
-	if (result) {
-		if (result.descriptionText)
-			displayInfoLog(result.descriptionText)
-		displayDebugLog("Creating event $result")
-		return createEvent(result)
-	} else
-		return [:]
+	return [:]
 }
 
 // Reverses order of bytes in hex string
@@ -131,13 +127,12 @@ def reverseHexString(hexString) {
 }
 
 // Calculate temperature with 0.01 precision in C or F unit as set by hub location settings
-private parseTemperature(hexString) {
+private parseTemperature(hexString, checkin) {
 	float temp = hexStrToSignedInt(hexString)/100
 	def tempScale = location.temperatureScale
 	def debugText = "Reported temperature: raw = $temp°C"
 	if (temp < -50) {
 		log.warn "${device.displayName}: Out-of-bounds temperature value received. Battery voltage may be too low."
-		return ""
 	} else {
 		if (tempScale == "F") {
 			temp = ((temp * 1.8) + 32)
@@ -149,18 +144,18 @@ private parseTemperature(hexString) {
 		}
 		displayDebugLog(debugText)
 		temp = temp.round(2)
-		return [
+		generateEvent([
 			name: 'temperature',
 			value: temp,
 			unit: "°$tempScale",
 			descriptionText: "Temperature is $temp°$tempScale",
 			translatable:true
-		]
+			], checkin)
 	}
 }
 
 // Calculate humidity with 0.1 precision
-private parseHumidity(hexString) {
+private parseHumidity(hexString, checkin) {
 	float humidity = Integer.parseInt(hexString,16)/100
 	def debugText = "Reported humidity: raw = $humidity"
 	if (humidity > 100) {
@@ -173,37 +168,36 @@ private parseHumidity(hexString) {
 		}
 		displayDebugLog(debugText)
 		humidity = humidity.round(1)
-		return [
+		generateEvent([
 			name: 'humidity',
 			value: humidity,
 			unit: "%",
-			descriptionText: "Humidity is ${humidity}%",
-		]
+			descriptionText: "Humidity is $humidity%"
+			], checkin)
 	}
 }
 
 // Parse pressure report
-private parsePressure(hexString) {
-	float pressureval = Integer.parseInt(hexString[0..3], 16)
-	def debugText = "Reported pressure: raw = $pressureval"
+private parsePressure(float pressureval, checkin) {
+	def debugText = "Reported pressure: raw = $pressureval Pa"
 	if (!(pressureUnits)) {
 		pressureUnits = "mbar"
 	}
 	switch (pressureUnits) {
 		case "mbar":
-			pressureval = (pressureval/10) as Float
+			pressureval = (pressureval/100) as Float
 			pressureval = pressureval.round(1);
 			break;
 		case "kPa":
-			pressureval = (pressureval/100) as Float
+			pressureval = (pressureval/1000) as Float
 			pressureval = pressureval.round(2);
 			break;
 		case "inHg":
-			pressureval = (((pressureval/10) as Float) * 0.0295300)
+			pressureval = (((pressureval/100) as Float) * 0.0295300)
 			pressureval = pressureval.round(2);
 			break;
 		case "mmHg":
-			pressureval = (((pressureval/10) as Float) * 0.750062)
+			pressureval = (((pressureval/100) as Float) * 0.750062)
 			pressureval = pressureval.round(2);
 			break;
 	}
@@ -213,69 +207,128 @@ private parsePressure(hexString) {
 		pressureval = (pressureval + pressOffset)
 	}
 	displayDebugLog(debugText)
-	pressureval = pressureval.round(2);
-	return [
+	pressureval = pressureval.round(2)
+	generateEvent([
 		name: 'pressureMeasurement',
 		value: pressureval,
 		unit: pressureUnits,
-		descriptionText: "Pressure is ${pressureval} ${pressureUnits}"
-	]
-}
-
-def parseCheckinMessage(hexString) {
-	displayDebugLog("Checkin message raw hex string = ${hexString}")
-	return parseBattery(hexString)
+		descriptionText: "Pressure is $pressureval $pressureUnits"
+		], checkin)
 }
 
 // Convert raw 4 digit integer voltage value into percentage based on minVolts/maxVolts range
 private parseBattery(hexString) {
-	def hexBattery = (hexString[8..9] + hexString[6..7])
-	displayDebugLog("Battery parse string = ${hexBattery}")
-	def rawValue = Integer.parseInt(hexBattery,16)
-	def rawVolts = rawValue / 1000
+	def rawVolts = Integer.parseInt(hexString,16) / 1000
 	def minVolts = voltsmin ? voltsmin : 2.8
 	def maxVolts = voltsmax ? voltsmax : 3.05
+	displayDebugLog("Battery report: $rawVolts Volts, calculating level based on min/max range of $minVolts to $maxVolts")
 	def pct = (rawVolts - minVolts) / (maxVolts - minVolts)
 	def roundedPct = Math.min(100, Math.round(pct * 100))
-	displayDebugLog("Battery report: $rawVolts Volts, calculating level based on min/max range of $minVolts to $maxVolts")
-	def descText = "Battery level is $roundedPct% ($rawVolts Volts)"
-	return [
+	generateEvent([
 		name: 'battery',
 		value: roundedPct,
 		unit: "%",
-		descriptionText: descText
-	]
+		descriptionText: "Battery level is $roundedPct% ($rawVolts Volts)"
+		], false)
+}
 
-	// lastCheckinEpoch is for apps that can use Epoch time/date and lastCheckinTime can be used with Hubitat Dashboard
+def generateEvent(eventResult, checkin) {
+	eventResult.descriptionText += checkin ? " (check-in report)" : ""
+	eventResult = (recordAllReadings || checkin || ((eventResult.name == battery) && recordAllBattery)) ? (eventResult + [isStateChange: true]) : eventResult
+	displayDebugLog("Creating event $eventResult")
+	sendEvent(eventResult)
+	displayInfoLog(eventResult.descriptionText)
+}
+
+def parseCheckinMessage(hexString) {
 	if (lastCheckinEnable) {
+	// lastCheckinEpoch is for apps that can use Epoch time/date and lastCheckinTime can be used with Hubitat Dashboard
 		sendEvent(name: "lastCheckinEpoch", value: now())
 		sendEvent(name: "lastCheckinTime", value: new Date().toLocaleString())
 	}
-	return result
+	displayDebugLog("Received check-in message")
+	def result
+	// First byte of hexString is UINT8 of payload length in bytes, so it is skipped
+	def strPosition = 2
+	def strLength = hexString.size() - 2
+	while (strPosition < strLength) {
+		def dataTag = Integer.parseInt(hexString[strPosition++..strPosition++], 16)  // Each attribute of the check-in message payload is preceded by a unique 1-byte tag value
+		def dataType = Integer.parseInt(hexString[strPosition++..strPosition++], 16)  // After each attribute tag, the following byte gives the data type of the attribute data
+		def dataLength = DataType.getLength(dataType)  // This looks up the length of data for the determined data type
+		def dataPayload  // This is used to collect the payload data of each check-in message attribute
+		if (dataLength == null || dataLength == -1 || dataLength == 0) {  // A length of null or -1 means the data type is probably variable-length, and 0 length is invalid
+			displayDebugLog("Check-in message contains unsupported dataType 0x${Integer.toHexString(dataType)} for dataTag 0x${Integer.toHexString(dataTag)} with dataLength $dataLength")
+			return
+		} else {
+			if (strPosition > (strLength - dataLength)) {
+				displayDebugLog("Ran out of data before finishing parse of check-in message")
+				return
+			}
+			dataPayload = hexString[strPosition++..(strPosition+=(dataLength * 2) - 1)-1]  // Collect attribute tag payload according to data length of its data type
+			dataPayload = reverseHexString(dataPayload)  // Reverse order of bytes for big endian payload
+			def dataDebug1 = "Check-in message: Found dataTag 0x${Integer.toHexString(dataTag)}"
+			def dataDebug2 = "dataType 0x${Integer.toHexString(dataType)}, dataLength $dataLength, dataPayload $dataPayload"
+			switch (dataTag) {
+				case 0x01:  // Battery voltage
+					displayDebugLog("$dataDebug1 (battery), $dataDebug2")
+					parseBattery(dataPayload)
+					break
+				case 0x05:  // RSSI dB
+					def convertedPayload = Integer.parseInt(dataPayload,16)
+					displayDebugLog("$dataDebug1 (RSSI dB), $dataDebug2 ($convertedPayload)")
+					state.RSSI = convertedPayload
+					break
+				case 0x06:  // LQI
+					def convertedPayload = Integer.parseInt(dataPayload,16)
+					displayDebugLog("$dataDebug1 (LQI), $dataDebug2 ($convertedPayload)")
+					state.LQI = convertedPayload
+					break
+				case 0x64:  // Temperature in Celcius
+					displayDebugLog("$dataDebug1 (temperature), $dataDebug2")
+					parseTemperature(dataPayload, true)
+					break
+				case 0x65:  // Relative humidity
+					displayDebugLog("$dataDebug1 (humidity), $dataDebug2")
+					parseHumidity(dataPayload, true)
+					break
+				case 0x66:  // Atmospheric pressure
+					displayDebugLog("$dataDebug1 (pressure), $dataDebug2")
+					parsePressure(Integer.parseInt(dataPayload,16), true)
+					break
+				case 0x0A:  // ZigBee parent DNI (device network identifier)
+					displayDebugLog("$dataDebug1 (ZigBee parent DNI), $dataDebug2")
+					state.zigbeeParentDNI = dataPayload
+					break
+				default:
+					displayDebugLog("$dataDebug1 (unknown), $dataDebug2")
+			}
+		}
+	}
 }
 
 // installed() runs just after a sensor is paired
 def installed() {
 	displayDebugLog("Installing")
-	state.prefsSetCount = 0
 	init()
 }
 
-// configure() runs after installed() when a sensor is paired
+// configure() runs after installed() when a sensor is paired or re-joined
 def configure() {
 	displayInfoLog("Configuring")
 	init()
-	state.prefsSetCount = 1
+	device.updateSetting("infoLogging",[value:"true",type:"bool"])
+	device.updateSetting("debugLogging",[value:"true",type:"bool"])
+	displayInfoLog("Info and debug message logging automatically enabled for 2 hours. This can be overidden by saving preferences for this device.")
+	runIn(7200, stopAutoLogging, [data: [stopLogging: true]])
 }
 
 // updated() will run every time user saves preferences
 def updated() {
 	displayInfoLog("Updating preference settings")
 	init()
-	if (lastCheckinEnable)
-		displayInfoLog("Last checkin events enabled")
 	displayInfoLog("Info message logging enabled")
 	displayDebugLog("Debug message logging enabled")
+	runIn(2, stopAutoLogging, [data: [stopLogging: false]])
 }
 
 def init() {
@@ -292,6 +345,14 @@ def init() {
 	}
 	if (!device.currentValue('batteryLastReplaced'))
 		resetBatteryReplacedDate(true)
+}
+
+def stopAutoLogging(data) {
+	if (data.stopLogging) {
+		displayInfoLog("Automatic info and debug message logging now finished")
+		device.updateSetting("infoLogging",[value:"false",type:"bool"])
+		device.updateSetting("debugLogging",[value:"false",type:"bool"])
+	}
 }
 
 //Reset the batteryLastReplaced date to current date
